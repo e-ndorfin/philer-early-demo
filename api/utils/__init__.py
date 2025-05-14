0bc1b43423a48c2a09aae8ebab873b518b7a8a8a
e-ndorfin <zach.tang@mail.utoronto.ca>
2025-05-14 23:02:21 +0800

    stt delete

--- api/utils/__init__.py
+++ api/utils/__init__.py
@@ -3,6 +3,6 @@ from .date_utils import *
 from .extraction_utils import *
 from .json_utils import *
 from .question_utils import *
-from .stt import *
+# from .stt import *
 from .tts import *
 from .twilio_utils import *
